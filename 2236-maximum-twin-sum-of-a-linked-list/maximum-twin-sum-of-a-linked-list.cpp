f60c69ac19aaeb3414353e58e25cbb37bc4266ff
Sriyansh srivastava <84917748+Sriyansh2001@users.noreply.github.com>
2023-08-10 10:32:14 +0530

    Time: 186 ms (93.73%) | Memory: 116.6 MB (63.50%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -11,34 +11,34 @@
 class Solution {
     ListNode* reverselist(ListNode* head) {
         ListNode* curr = head, *next = head, *prev = nullptr;
-
+        // reverse linklist.
         while(curr) {
             next = curr->next;
             curr->next = prev;
             prev = curr;
             curr = next;
         }
-
+        // returing head pointer of reversed linklist.
         return prev;
     }
 public:
     int pairSum(ListNode* head) {
         ListNode* slow = head, *fast = head;
         int result = 0;
-        
+        // Find the middle element.
         while(fast && fast->next) {
             slow = slow->next;
             fast = fast->next->next;
         }
-
+        // Reverse Second half
         ListNode* headptr = reverselist(slow);
-
+        // Track the answer.
         while(head && headptr) {
             result = max(result,head->val+headptr->val);
             head = head->next;
             headptr = headptr->next;
         }
-
+        // returing result.
         return result;
     }
 };
