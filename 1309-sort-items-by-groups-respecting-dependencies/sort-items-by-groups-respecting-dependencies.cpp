e670e4d8542f2427344af69b826621cdb6e2b96d
Sriyansh srivastava <84917748+Sriyansh2001@users.noreply.github.com>
2023-08-21 00:40:31 +0530

    Time: 96 ms (79.94%) | Memory: 84.5 MB (17.44%) - LeetSync

--- 1309-sort-items-by-groups-respecting-dependencies/sort-items-by-groups-respecting-dependencies.cpp
+++ 1309-sort-items-by-groups-respecting-dependencies/sort-items-by-groups-respecting-dependencies.cpp
@@ -1,63 +1,7 @@
 class Solution {
 public:
-    vector<int> toposort(vector<vector<int>> &g,vector<int> &vis,int res) {
-        int n = g.size();
-        queue<int> q;
-        
-        for(int i=0 ;  i<n ; ++i) {
-            if(vis[i] == 0) {
-                q.push(i);
-            }
-        }
-
+    vector<int> topologicalsort(queue<int> &q,vector<vector<int>> &g,vector<int> &indg) {
         vector<int> ans;
-
-        while(!q.empty()) {
-            int k = q.front(); q.pop();
-            ans.push_back(k);
-            for(auto i:g[k]) {
-                vis[i]-=1;
-                if(vis[i] == 0) {
-                    q.push(i);
-                }
-            }
-        }
-
-        if(ans.size()!=res) return {}; return ans;
-    }
-
-    // vector<int> solve(int n,vector<vector<int>> &beforeItems,vector<int> &group,int val) {
-    //     vector<int> vis(n,0);
-    //     vector<vector<int>> g(n);
-    //     int count = 0;
-
-    //     for(int i=0 ; i<n ; ++i) {
-    //         if(group[i] == val) {
-    //             count+=1;
-    //             for(int j=0 ; j<beforeItems[i].size() ; ++j) {
-    //                 if(group[beforeItems[i][j]] == val) {
-    //                     vis[i] += 1;
-    //                     g[beforeItems[i][j]].push_back(i);
-    //                 }
-    //             }
-    //         }
-    //         else vis[i] = INT_MAX;
-    //     }
-    //     return toposort(g,vis,count);
-    // }
-
-    vector<int> solve(vector<vector<int>> &g,vector<int> &val,vector<int> &indg) {
-        queue<int> q;
-
-        for(auto i:val) {
-            if(indg[i] == 0) {
-                q.push(i);
-            }
-        }
-
-        
-        vector<int> ans;
-
         while(!q.empty()) {
             int k = q.front(); q.pop();
             ans.push_back(k);
@@ -68,9 +12,30 @@ public:
                 }
             }
         }
+        return ans;
+    }
 
-        if(val.size() == ans.size()) return ans;
-        return {};
+    vector<int> toposortgrouptogether(vector<vector<int>> &g,vector<int> &vis) {
+        int n = g.size();
+        queue<int> q;
+        for(int i=0 ;  i<n ; ++i) {
+            if(vis[i] == 0) {
+                q.push(i);
+            }
+        }
+        vector<int> ans=topologicalsort(q,g,vis);
+        return ans;
+    }
+
+    vector<int> toposorteverysinglegroupelement(vector<vector<int>> &g,vector<int> &val,vector<int> &indg) {
+        queue<int> q;
+        for(auto i:val) {
+            if(indg[i] == 0) {
+                q.push(i);
+            }
+        }
+        vector<int> ans = topologicalsort(q,g,indg);
+        return ans;
     }
 
     vector<int> sortItems(int n, int m, vector<int>& group, vector<vector<int>>& beforeItems) {
@@ -78,6 +43,8 @@ public:
         vector<int> vis(n+m,INT_MAX);
         vector<vector<int>> countspe(n);
         vector<int> indg(n,0);
+        vector<vector<int>> selfgroup(n);
+
         for(int i=0 ; i<n ; ++i) {
             if(group[i] != -1) {
                 vis[n+group[i]] = 0;
@@ -85,13 +52,10 @@ public:
             }
             vis[i] = 0;
         }
-        int count = 0;
-        set<int> st;
-        vector<vector<int>> selfgroup(n);
+        
         for(int i=0 ; i<n ; ++i) {
             if(group[i] != -1) {
                 countspe[group[i]].push_back(i);
-                st.insert(group[i]);
                 for(int j=0 ; j<beforeItems[i].size() ; ++j) {
                     int ind = group[beforeItems[i][j]] == -1? beforeItems[i][j] : n+group[beforeItems[i][j]];
                     if(group[beforeItems[i][j]] != group[i]) {
@@ -105,7 +69,6 @@ public:
                 }
             }
             else {
-                count+=1;
                 for(int j=0 ; j<beforeItems[i].size() ; ++j) {
                     vis[i]+=1;
                     int ind = group[beforeItems[i][j]] == -1? beforeItems[i][j] : n+group[beforeItems[i][j]];
@@ -114,21 +77,17 @@ public:
             }
         }
 
-        vector<int> res = toposort(g,vis,count+st.size());
+        vector<int> res = toposortgrouptogether(g,vis);
 
-        if(res.size() == 0) {
-            return {};
-        }
-
-        map<int,vector<int>> mp;
-        for(int i=n ; i<n+m ; ++i) {
-            mp[i] = solve(selfgroup,countspe[i-n],indg);
+        vector<vector<int>> mp(m);
+        for(int i=0 ; i<m ; ++i) {
+            mp[i] = toposorteverysinglegroupelement(selfgroup,countspe[i],indg);
         }
 
         vector<int> ans;
         for(int i=0 ; i<res.size() ; ++i) {
-            if(mp.find(res[i])!=mp.end()) {
-                for(auto j:mp[res[i]]) ans.push_back(j);
+            if(res[i]>=n) {
+                for(auto j:mp[res[i]-n]) ans.push_back(j);
             }
             else {
                 ans.push_back(res[i]);
