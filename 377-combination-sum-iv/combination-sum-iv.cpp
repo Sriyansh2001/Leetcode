a7141d221efa50fff1730da035bcd671c67293ad
Sriyansh srivastava <84917748+Sriyansh2001@users.noreply.github.com>
2023-09-09 19:59:09 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (20.91%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -2,7 +2,6 @@ class Solution {
     vector<int> dp;
 public:
     int solve(vector<int> &nums,int target) {
-        if(target == 0) return 1;
         if(target < 0) return 0;
         if(dp[target] != -1) return dp[target];
         dp[target] = 0;
@@ -14,6 +13,7 @@ public:
  
     int combinationSum4(vector<int>& nums, int target) {
         dp.resize(target+1,-1);
+        dp[0]=1;
         return solve(nums,target);
     }
 };
