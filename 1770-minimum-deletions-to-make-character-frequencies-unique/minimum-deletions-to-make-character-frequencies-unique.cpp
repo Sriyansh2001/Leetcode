ce55f82723576771fdbebd319238a3ce724b5505
Sriyansh srivastava <84917748+Sriyansh2001@users.noreply.github.com>
2023-09-12 07:27:50 +0530

    Time: 123 ms (15.24%) | Memory: 18.1 MB (8.68%) - LeetSync

---
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        map<char,int> freq;
+        for(auto i:s) freq[i]+=1;
+        vector<int> fre;
+        for(auto i:freq) fre.push_back(i.second); 
+        sort(fre.begin(),fre.end());
+        int n = fre.size(), j = 0;
+        stack<int> st;
+        int answer = 0, mx = fre[n-1];
+        for(int i=1 ; i<=mx && j<n ; ++i) {
+            if(fre[j] != i) {
+                st.push(i);
+            } else {
+                int val = fre[j], count = 0;
+                while(j<n && fre[j] == val) {
+                    j+=1;
+                    count += 1;
+                } count-=1;
+                while(count > 0) {
+                    if(st.size()) {
+                        answer += (i-st.top());
+                        st.pop();
+                    }
+                    else answer += i;
+                    count-=1;
+                }
+            }
+        }
+        return answer;
+    }
+};
