082face37f4e061a61fb288530f927034cacf24f
Sriyansh srivastava <84917748+Sriyansh2001@users.noreply.github.com>
2023-11-19 22:25:30 +0530

    Time: 544 ms (5.22%) | Memory: 130.5 MB (12.58%) - LeetSync

---
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reductionOperations(vector<int>& nums) {
+        map<int,int> mp;
+        for(auto i:nums) {
+            mp[i]+=1;
+        }
+        priority_queue<int> q;
+        for(auto i:mp) {
+            q.push(i.first);
+        }
+        int ans = 0;
+        while(q.size() > 1) {
+            int k = q.top();
+            q.pop();
+            ans += mp[k];
+            if(q.size()) {
+                mp[q.top()] += mp[k];
+            }
+        }
+        return ans;
+    }
+};
