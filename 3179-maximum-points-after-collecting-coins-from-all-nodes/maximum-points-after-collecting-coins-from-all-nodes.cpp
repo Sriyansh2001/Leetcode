4975c89cb3fdaf46515a203c903e2d189852dcfc
Sriyansh srivastava <84917748+Sriyansh2001@users.noreply.github.com>
2023-12-14 10:40:52 +0530

    Time: 613 ms (48.55%) | Memory: 229.2 MB (52.48%) - LeetSync

--- 3179-maximum-points-after-collecting-coins-from-all-nodes/maximum-points-after-collecting-coins-from-all-nodes.cpp
+++ 3179-maximum-points-after-collecting-coins-from-all-nodes/maximum-points-after-collecting-coins-from-all-nodes.cpp
@@ -6,28 +6,30 @@ public:
         if(dp[ind][floor] != -1) {
             return dp[ind][floor];
         }
+
         int ans1 = ((coins[ind]/(pow(2,floor))/2));
         int ans2 = (coins[ind]/(pow(2,floor)))-k;
-        int a=0,b=0;
+        
         for(auto i:g[ind]) {
-            if(i == par) {
-                continue;
-            }
-            a += solve(g,i,ind,(floor < 14 ? floor+1: floor),coins,k);
-            b += solve(g,i,ind,floor,coins,k);
+            if(i == par) continue;
+            
+            ans1 += solve(g,i,ind,min(14,floor+1),coins,k);
+            ans2 += solve(g,i,ind,floor,coins,k);
         }
         
-        return dp[ind][floor] = max(ans1+a,ans2+b);
+        return dp[ind][floor] = max(ans1,ans2);
     }
 
     int maximumPoints(vector<vector<int>>& edges, vector<int>& coins, int k) {
         int n = edges.size();
         vector<vector<int>> g(n+1);
-        dp.resize(n+10,vector<int> (15,-1));
+        dp.resize(n+1,vector<int> (15,-1));
+
         for(int i=0 ; i<n ; ++i) {
             g[edges[i][0]].push_back(edges[i][1]);
             g[edges[i][1]].push_back(edges[i][0]);
         }
+        
         return solve(g,0,-1,0,coins,k);
     }
 };
