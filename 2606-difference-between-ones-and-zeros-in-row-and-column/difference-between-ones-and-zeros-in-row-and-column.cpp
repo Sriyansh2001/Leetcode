d01f0b572cb3714b7e811e57fd0f14e30dfc05e8
Sriyansh srivastava <84917748+Sriyansh2001@users.noreply.github.com>
2023-12-14 09:48:24 +0530

    Time: 182 ms (86.77%) | Memory: 117.5 MB (76.35%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
         int n = grid.size(),m = grid[0].size();
-        vector<int> rowOne(n,0),colOne(m,0),rowZero(n,0),colZero(m,0);
+        vector<int> rowOne(n,0),colOne(m,0);
         for(int i=0 ; i<n ; ++i) {
             for(int j=0 ; j<m ; ++j) {
-                rowOne[i] += (grid[i][j] == 1);
-                rowZero[i] += (grid[i][j] == 0);
-                colOne[j] += (grid[i][j] == 1);
-                colZero[j] += (grid[i][j] == 0);
+                rowOne[i] += grid[i][j];
+                colOne[j] += grid[i][j];
             }
         }
         vector<vector<int>> ans(n,vector<int> (m,0));
         for(int i=0 ; i<n ; ++i) {
             for(int j=0 ; j<m ; ++j) {
-                ans[i][j] = rowOne[i]+colOne[j]-rowZero[i]-colZero[j];
+                ans[i][j] = (2*rowOne[i]-n)+(2*colOne[j]-m);
             }
         }
         return ans;
