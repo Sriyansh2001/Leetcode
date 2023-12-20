571462dd67582b26c7f8cabf199193055d794485
Sriyansh srivastava <84917748+Sriyansh2001@users.noreply.github.com>
2023-12-20 10:23:27 +0530

    Time: 160 ms (61.94%) | Memory: 51.9 MB (97.57%) - LeetSync

--- 3107-maximum-spending-after-buying-items/maximum-spending-after-buying-items.cpp
+++ 3107-maximum-spending-after-buying-items/maximum-spending-after-buying-items.cpp
@@ -7,16 +7,13 @@ public:
         long long d = 1,ans=0;
         vector<int> v(n,m-1);
         
-        while(1) {
+        for(int j=0 ; j<m*n ; ++j) {
             mn = -1,val=INT_MAX;
             for(int i=0 ; i<n ; ++i) {
                 if(v[i]>=0 && val > values[i][v[i]]) {
                     mn = i;
                     val = values[i][v[i]];
                 }
-            }
-            if(mn == -1) {
-                return ans;
             }
             v[mn]-=1;
             ans = ans + d * val;
