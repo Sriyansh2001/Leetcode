a10bcea79e4106c502c7f046a28fc3f79ab7be83
Sriyansh srivastava <84917748+Sriyansh2001@users.noreply.github.com>
2023-12-20 10:14:12 +0530

    Time: 977 ms (5.01%) | Memory: 111.9 MB (6.03%) - LeetSync

---
+++ 3107-maximum-spending-after-buying-items/maximum-spending-after-buying-items.cpp
@@ -0,0 +1,34 @@
+class cmp {
+    public:
+    bool operator()(vector<int> &a,vector<int> &b) {
+        return a[0]>b[0];
+    }
+};
+
+class Solution {
+public:
+    long long maxSpending(vector<vector<int>>& values) {
+        int n = values.size();
+        int m = values[0].size();
+        priority_queue<vector<int>,vector<vector<int>> , cmp> q;
+        long long d = 1,ans=0;
+
+        for(int i=0 ; i<n ; ++i) {
+            q.push({values[i][m-1],i,m-1});
+        }
+
+        while(!q.empty()) {
+            vector<int> k = q.top(); q.pop();
+            long long val = k[0];
+            int ind1 = k[1];
+            int ind2 = k[2]-1;
+            if(ind2 >= 0) {
+                q.push({values[ind1][ind2],ind1,ind2});
+            }
+            ans = ans + values[ind1][ind2+1] * d;
+            cout << ans << "\n";
+            d+=1;
+        }
+        return ans;
+    }
+};
