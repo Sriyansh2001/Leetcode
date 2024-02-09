d8f5038863756c1e2d0951540ac0109673bec0cd
Sriyansh srivastava <84917748+Sriyansh2001@users.noreply.github.com>
2024-02-09 22:25:44 +0530

    Time: 55 ms (5.33%) | Memory: 31 MB (66.93%) - LeetSync

---
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int calbit(int x) {
+        int ans = 0;
+        while(x) {
+            if(x&1) ans += 1;
+            x/=2;
+        }
+        return ans;
+    }
+
+    void bitsort(vector<int> &nums) {
+        int n = nums.size();
+        for(int i=0 ; i<n ; ++i) {
+            for(int j=i-1 ; j>=0 ; --j) {
+                if(calbit(nums[j]) == calbit(nums[j+1])) {
+                    if(nums[j] > nums[j+1]) {
+                        int temp = nums[j];
+                        nums[j] = nums[j+1];
+                        nums[j+1] = temp;
+                    }
+                }
+                else break;
+            }
+        }
+    }
+
+    bool check_sort(vector<int> &nums) {
+        int n = nums.size();
+        for(int i=0 ; i<n-1 ; ++i) {
+            if(nums[i]>nums[i+1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool canSortArray(vector<int>& nums) {
+        bitsort(nums);
+        for(auto i:nums) {
+            cout << i << " ";
+        }
+        return check_sort(nums);
+    }
+};
