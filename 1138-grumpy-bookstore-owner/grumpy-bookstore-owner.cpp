c0688307d110eec2f89819f4940056c3cd5c62fa
Sriyansh srivastava <84917748+Sriyansh2001@users.noreply.github.com>
2024-06-22 02:06:19 +0530

    Time: 22 ms (72.62%) | Memory: 34.4 MB (48.80%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -3,14 +3,14 @@ public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
         int ans = 0, extra = 0, extra_max = 0;
         int n = grumpy.size();
-        for(int i=n-1 ; i>=0 ; --i) {
+        int k = n - 1 + minutes;
+        for(int i=n-1 ; i>=0 ; --i, --k) {
             if(!grumpy[i]) {
                 ans += customers[i];
             } 
             else {
                 extra = customers[i] + extra;   
             }   
-            int k = i+minutes;
             if(k < n && grumpy[k]) {
                 extra -= customers[k];
             }
