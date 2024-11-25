e3125e5ba4ce07a451b5411dfc43cc5bdc0103cf
Sriyansh srivastava <84917748+Sriyansh2001@users.noreply.github.com>
2024-11-26 00:30:15 +0530

    Time: 1206 ms (5.62%) | Memory: 96.7 MB (5.14%) - LeetSync

---
+++ 787-sliding-puzzle/sliding-puzzle.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    map<string,int> hashMap;
+    int n = 2,m = 3;
+    int tempi, tempj, temp;
+    vector<vector<int>> dir = {{1,0},{0,1},{-1,0},{0,-1}};
+
+    bool checkIsPass(vector<vector<int>> &board) {
+        int start = 1;
+        for(int i=0 ; i<n ; ++i) {
+            for(int j=0 ; j<m ; ++j) {
+                if(board[i][j] == start) {
+                    start++;
+                    if(start == 6) {
+                        return true;
+                    }
+                } else return false;
+            }
+        }
+        return true;
+    }
+
+    bool checkSwapPossible(int i, int j) {
+        return i >= 0 && i < n && j >= 0 && j < m;
+    }
+
+    string convertIntoString(vector<vector<int>> &board) {
+        string s;
+        for(int i=0 ; i<n ; ++i) {
+            for(int j=0 ; j<m ; ++j) {
+                s.push_back(board[i][j] + '0');
+            }
+        }
+        return s;
+    }
+
+    int solve(vector<vector<int>> &board, int i,int j, int moves) {
+        string currentBoard = convertIntoString(board);
+        if(hashMap.find(currentBoard) != hashMap.end() && hashMap[currentBoard] < moves) {
+            return hashMap[currentBoard];
+        }
+        hashMap[currentBoard] = moves;
+        int res = 1e6+1;
+        for(auto d: dir) {
+            int tempi = i+d[0];
+            int tempj = j+d[1];
+            if(checkSwapPossible(tempi,tempj)) {
+                temp = board[i][j];
+                board[i][j] = board[tempi][tempj];
+                board[tempi][tempj] = temp;
+                res = min(res, solve(board, tempi, tempj,moves + 1));
+                temp = board[i][j];
+                board[i][j] = board[tempi][tempj];
+                board[tempi][tempj] = temp;
+            }
+        }
+        return res;
+    }
+
+    int slidingPuzzle(vector<vector<int>>& board) {
+        for(int i=0 ; i<n ; ++i) {
+            for(int j=0 ; j<m ; ++j) {
+                if(board[i][j] == 0) {
+                    tempi = i;
+                    tempj = j;
+                }
+            }
+        } 
+        int res = solve(board,tempi,tempj,0);
+
+        if(hashMap.find("123450") == hashMap.end()) {
+            return -1;
+        } return hashMap["123450"];
+    }
+};
