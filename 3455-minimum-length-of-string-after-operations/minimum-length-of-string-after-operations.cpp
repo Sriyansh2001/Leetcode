c5a8e86005084843eb0b36211cd54fb0b952d265
Sriyansh srivastava <84917748+Sriyansh2001@users.noreply.github.com>
2025-01-13 23:05:29 +0530

    Time: 110 ms (26.21%) | Memory: 32.7 MB (10.89%) - LeetSync

---
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        map<char, int> mp;
+        int n = s.length();
+        for(int i=0 ; i<n ; ++i) {
+            mp[s[i]]++;
+        }
+        int length = n;
+        for(auto i:mp) {
+            int count = i.second;
+            if(count == 1 || count == 2) continue;
+            if(count % 2 == 0) {
+                length -= (count - 2);
+            } else {
+                length -= (count - 1);
+            }
+        }
+        return length;
+    }
+};
